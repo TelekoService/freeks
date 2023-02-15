b4ce8881f7942a6746f65644f97c9ad0bfca63fb
a411684a@yahoo.co.jp <a411684a@yahoo.co.jp>
2023-02-15 14:44:50 +0900

    課題提出

---
+++ 10-1/main.js
@@ -0,0 +1,33 @@
+// 問1:isEven関数を実行して、以下の配列から偶数だけが出力されるように実装してください。
+
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        isEven(numbers[i]);
+    }
+}
+function isEven(num) {
+    console.log(num + "は偶数です");
+}
+
+
+
+// 問2:以下の要件を満たすように実装してください。
+// 【要件】
+// ・インスタンス化した時にガソリンとナンバーがセットされるようなCarクラスを作成する
+// ・「ガソリンは〇〇です。ナンバーは△△です」と出力される「getNumGas」という関数を作成する
+
+
+class Car {
+    constructor(gasolene, number) {
+        this.gasolene = gasolene;
+        this.number = number;
+    }
+
+    getNumGas() {
+        console.log(`ガソリンは${this.gasolene}です。ナンバーは${this.number}です`);
+    }
+}
+let car = new Car("レギュラー", "あ12-34");
+car.getNumGas();
